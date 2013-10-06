3825cb6054b9d86c363f2dd1c91b647868403629
n200ok <node200ok@163.com>
2013-10-06 18:42:37 +0800

    for test

--- app.js
+++ app.js
@@ -27,6 +27,10 @@ wxbase({
 	wxToken: config.wxToken,
 	wxHandler: require('./lib/my-wx-handler').init(config, app)
 });
+// for test
+setInterval(function() {
+	console.log('Im alive');
+}, 1000 * 60 * 30);	// 30min
 
 app.use(express.static(config.publicDir));
 http.createServer(app).on('error', function(err) {
